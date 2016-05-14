219eef4738b1a1f2f8aa11c074f8f0b3b981649d
David Stinnette <dastinnette@gmail.com>
2016-05-14 11:47:47 -0600

    JS hello & leap complete

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,14 +1,12 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-var HelloWorld = function() {};
+var HelloWorld = function() {}
 
 HelloWorld.prototype.hello = function(input) {
-//
-// YOUR CODE GOES HERE
-//
-};
 
-module.exports = HelloWorld;
+  if (input === ''){
+    return 'Hello, World!'
+  }else{
+    return 'Hello, ' + input + '!'
+  }
+}
+
+module.exports = HelloWorld

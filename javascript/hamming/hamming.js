422452ba6771150742828100700ebab355d391bb
David Stinnette <dastinnette@gmail.com>
2016-05-15 19:51:16 -0600

    Hamming in JS complete

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,3 +1,20 @@
-function Hamming() {
+function Hamming() {}
 
-}
+Hamming.prototype.compute = function (strand1, strand2) {
+  var hammingDistance = 0;
+  var splitStrand1 = strand1.split("");
+  var splitStrand2 = strand2.split("");
+
+  if(strand1.length != strand2.length){
+    throw "DNA strands must be of equal length."
+  } else {
+      for(var i = 0; i < strand1.length; i++) {
+        if(splitStrand1[i] != splitStrand2[i]) {
+          hammingDistance ++;
+        }
+      }
+    }
+  return hammingDistance;
+};
+
+module.exports = Hamming;

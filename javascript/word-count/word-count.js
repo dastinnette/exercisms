87a1638a106fe578926e1af7a223ca4a03b276c5
David Stinnette <dastinnette@gmail.com>
2016-05-18 10:20:57 -0600

    JS word count complete except for leading whitespace error

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,17 @@
+function Words() {}
+
+Words.prototype.count = function(words) {
+  var wordCount = {};
+  var splitWords = words.toLowerCase().split(/\s+/);
+
+  splitWords.forEach(function(word) {
+    if(wordCount[word] && wordCount.hasOwnProperty(word)) {
+      wordCount[word]++;
+    } else {
+      wordCount[word] = 1;
+    }
+  });
+  return wordCount;
+};
+
+module.exports = Words;

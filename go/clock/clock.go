4faea1eb6c8cf33c445ea80b0e960681da33c887
David Stinnette <dastinnette@gmail.com>
2016-07-26 14:35:05 -0700

    All tests passing converting clock to int type

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,71 +1,23 @@
 package clock
 
-import (
-  "strconv"
-)
+import "fmt"
 
 const testVersion = 4
 
-type Clock struct {
-  hours, minutes int
-}
+type Clock int
 
 func New(hour, minute int) Clock {
-  clock := Clock{};
-  clock.hours = hour;
-  clock.minutes = minute;
-  return clock;
+	time := (hour*60 + minute) % (60 * 24)
+	if time < 0 {
+		time += 60 * 24
+	}
+	return Clock(time)
 }
 
 func (clock Clock) String() string {
-  hour := "";
-  minute := "";
-  numberDays := 1;
-  numberHours := 1;
-
-  if clock.minutes > 59 {
-    numberHours = clock.minutes/60;
-    clock.minutes -= (numberHours * 60);
-    clock.hours += (numberHours);
-  } else if clock.minutes < -59 {
-    numberHours = clock.minutes/-60;
-    clock.minutes += (numberHours *60);
-    clock.hours -= numberHours;
-  }
-
-  if clock.minutes < 0 {
-    clock.hours --;
-    clock.minutes = 60 + clock.minutes;
-  }
-
-  if clock.hours > 23 {
-    numberDays = clock.hours/24;
-    clock.hours -= (numberDays * 24);
-  } else if clock.hours < -23 {
-    numberDays = clock.hours/-24;
-    clock.hours += (numberDays * 24);
-  }
-
-  if clock.hours < 0 {
-    clock.hours = 24 + clock.hours;
-  }
-
-  if clock.hours > 9 {
-    hour = strconv.Itoa(clock.hours);
-  } else {
-    hour = "0" + strconv.Itoa(clock.hours);
-  }
-
-  if clock.minutes > 9 {
-    minute = strconv.Itoa(clock.minutes);
-  } else {
-    minute = "0" + strconv.Itoa(clock.minutes);
-  }
-  return hour + ":" + minute;
-
+	return fmt.Sprintf("%02d:%02d", clock/60, clock%60)
 }
 
 func (clock Clock) Add(minutes int) Clock {
-  clock.minutes += minutes;
-  return clock;
+	return New(0, int(clock) + minutes)
 }
